d8116e6808017ae7d462be9b12ee997d94b296ff
J. Paterson <contact@jpatersondev.com>
2019-09-10 12:20:50 -0700

    add db rake task

--- dino.rb
+++ dino.rb
@@ -1,5 +1,8 @@
 require "sinatra"
+require "sinatra/activerecord"
 require_relative "db.rb"
+
+set :database, {adapter: "sqlite3", database: "dino.sqlite3"}
 
 get "/" do    
     @all_dinos = $ALL_DINOS
