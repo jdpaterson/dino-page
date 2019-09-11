fff8fbdefcf2cf1af2c832ba42230a60de9b6d0d
J. Paterson <contact@jpatersondev.com>
2019-09-10 19:52:57 -0700

    tux config complete. create Dino model

--- config.ru
+++ config.ru
@@ -1,6 +1,12 @@
-require File.expand_path('dino-app', File.dirname(__FILE__))
+db_options = {adapter: "sqlite3", database: "dino.sqlite3"}
+
 # Load Sinatra Framework (with AR)
 require 'sinatra'
+require "sinatra/base"
 require 'sinatra/activerecord'
+require File.expand_path('dino-app', File.dirname(__FILE__))
+require "./models/dino.rb"
+ActiveRecord::Base.establish_connection(db_options)
+set :database, db_options
 
 run Sinatra::Application
--- dino-app.rb
+++ dino-app.rb
@@ -1,21 +1,9 @@
-require "sinatra"
-require "sinatra/base"
-require "sinatra/activerecord"
-require_relative "db.rb"
-
-db_options = {adapter: "sqlite3", database: "dino.sqlite3"}
-set :database, db_options
-ActiveRecord::Base.establish_connection(db_options)
-class Dino < ActiveRecord::Base
-    
+get "/" do
+    @all_dinos = Dino.all
+    erb :index
 end
-class DinoApp < Sinatra::Base
-    get "/" do
-        @all_dinos = $ALL_DINOS
-        erb :index
-    end
 
-    get "/dinos/:id" do
-        "Hello world"
-    end
+get "/dinos/:id" do
+    "Hello world"
 end
+
